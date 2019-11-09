c4994c0ba60d2eaa02c752226f4118b89a573aa8
NoahLiu <thumbb13555@gmail.com>
2019-11-10 01:41:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/convertvalueexample/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.convertvalueexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG = MainActivity.class.getSimpleName()+"m";
+    Button btConvert;
+    EditText edInput,edBinary,edOctal,edDecimal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btConvert   = findViewById(R.id.button);
+        edInput     = findViewById(R.id.input);
+        edBinary    = findViewById(R.id.binary);
+        edOctal     = findViewById(R.id.octal);
+        edDecimal   = findViewById(R.id.decimal);
+        btConvert.setOnClickListener((v)->{convert();});
+
+    }
+
+    private void convert(){
+            if (edInput.getText().toString().length()>0){
+                long input = (short)Integer.parseInt(edInput.getText().toString(),16);
+                edDecimal.setText(String.valueOf(input));
+
+                if (Long.toBinaryString(input).length()<8){
+                    edBinary.setText(String.format("%08d",Integer.parseInt(Long.toBinaryString(input))));
+                }else if(input<0){
+                    edBinary.setText("-"+(Long.toBinaryString(~input+1)));
+                    edOctal.setText("-"+Long.toOctalString(~input+1));
+                }else{
+                    edBinary.setText(Long.toBinaryString(input));
+                    edOctal.setText(Long.toOctalString(input));
+                }
+
+            }
+
+        
+
+
+
+    }
+}
